65f209472fc588c2477a27de1580f286432ee9be
hjgeither <hjgeither@gmail.com>
2024-04-11 20:35:11 -0400

    updates from past couple of days

--- CAP1188/cap1188.c
+++ CAP1188/cap1188.c
@@ -9,8 +9,8 @@
 #define SLAVE_DEVICE_NAME ("CAP1188")
 #define CAP1188_SLAVE_ADDR (0x29)
 
-static struct i2c_adapter cap_i2c_adapter = NULL;
-static struct i2c_client cap_i2c_client = NULL;
+static struct i2c_adapter *cap_i2c_adapter = NULL;
+static struct i2c_client *cap_i2c_client = NULL;
 
 static int I2C_Write(unsigned char *buf, unsigned int len)
 {
@@ -20,7 +20,7 @@ static int I2C_Write(unsigned char *buf, unsigned int len)
 
 static int I2C_Read(unsigned char *out_buf, unsigned int len)
 {
-    int ret = i2c_master_recv(cap_i2c_client, out_buf, len)
+    int ret = i2c_master_recv(cap_i2c_client, out_buf, len);
     return ret;
 }
 
@@ -40,12 +40,12 @@ static int cap_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
     //CAP1188_Init();
     pr_info("CAP1188 probed!!!\n");
+    return 0;
 }
 
-static int cap_remove(struct i2c_client* client)
+static void cap_remove(struct i2c_client* client)
 {
     pr_info("CAP1188 Removed!!!\n");
-    return 0;
 }
 
 static const struct i2c_device_id cap_id[] = {
@@ -58,14 +58,14 @@ static struct i2c_driver cap_driver = {
     .driver = {
         .name = SLAVE_DEVICE_NAME,
         .owner = THIS_MODULE,
-    }
+    },
     .probe = cap_probe,
     .remove = cap_remove,
     .id_table = cap_id,
 };
 
 static struct i2c_board_info cap_i2c_board_info = {
-    I2C_BOARD_INFO(SLAVE_DEVICE_NAME, CAP1138_SLAVE_ADDR)
+    I2C_BOARD_INFO(SLAVE_DEVICE_NAME, CAP1188_SLAVE_ADDR)
 };
 
 static int __init cap_driver_init(void)
@@ -73,5 +73,33 @@ static int __init cap_driver_init(void)
     int ret = -1;
     cap_i2c_adapter = i2c_get_adapter(I2C_BUS_AVAILABLE);
 
-    if()
+    if(cap_i2c_adapter != NULL)
+    {
+	    cap_i2c_client = i2c_new_client_device(cap_i2c_adapter, &cap_i2c_board_info);
+
+	    if(cap_i2c_client != NULL)
+	    {
+		    i2c_add_driver(&cap_driver);
+		    ret = 0;
+	    }
+
+	    i2c_put_adapter(cap_i2c_adapter);
+    }
+    pr_info("Driver Added!!!!\n");
+    return ret;
 }
+
+static void __exit cap_driver_exit(void)
+{
+	i2c_unregister_device(cap_i2c_client);
+	i2c_del_driver(&cap_driver);
+	pr_info("Driver Removed!!!\n");
+}
+
+module_init(cap_driver_init);
+module_exit(cap_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ME");
+MODULE_DESCRIPTION("CAP1188 i2c driver");
+MODULE_VERSION("1.0");
